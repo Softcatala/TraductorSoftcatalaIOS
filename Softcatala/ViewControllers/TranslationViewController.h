ff6d400cd2fb7033ed134f975e151751e19b1378
Marcos-MBP <marcos.graup@gmail.com>
2014-03-05 23:56:39 +0100

    Storyboard y home view controller

---
+++ Softcatala/ViewControllers/TranslationViewController.h
@@ -0,0 +1,13 @@
+//
+//  TranslationViewController.h
+//  Softcatala
+//
+//  Created by marcos on 05/03/14.
+//  Copyright (c) 2014 Marcos Grau. All rights reserved.
+//
+
+
+
+@interface TranslationViewController : UIViewController
+
+@end
