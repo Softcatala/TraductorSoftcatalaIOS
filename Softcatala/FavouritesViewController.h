9d280e7c2d9f261c63a8a7f7bc486c7912ca3e4f
Marcos Grau <marcos.graup@gmail.com>
2014-05-23 16:12:33 +0200

    Añadida la pantalla de favoritos y el view controller

---
+++ Softcatala/FavouritesViewController.h
@@ -0,0 +1,13 @@
+//
+//  FavouritesViewController.h
+//  Softcatala
+//
+//  Created by Marcos Grau on 23/05/14.
+//  Copyright (c) 2014 Marcos Grau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FavouritesViewController : UIViewController
+
+@end
