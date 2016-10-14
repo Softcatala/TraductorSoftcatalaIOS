4c6c1aaa3cb283ffcf521cff383a959c3b4e8cf1
Marcos <marcos.graup@gmail.com>
2016-10-14 12:11:18 +0200

    Updated deprecated method call

--- Softcatala/Controls/ProgressHud.m
+++ Softcatala/Controls/ProgressHud.m
@@ -15,7 +15,7 @@
 {
     [SVProgressHUD setBackgroundColor:[UIColor clearColor]];
     [SVProgressHUD setForegroundColor:[UIColor colorWithRed:181.0/255.0 green:0.0 blue:39.0/255.0 alpha:1.0]];
-    [SVProgressHUD showWithMaskType:SVProgressHUDMaskTypeClear];
+    [SVProgressHUD setDefaultMaskType:SVProgressHUDMaskTypeClear];
 }
 
 + (void)dismiss
